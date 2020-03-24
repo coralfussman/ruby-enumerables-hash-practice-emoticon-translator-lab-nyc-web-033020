cd1897a853b36ac2d097fa82e1857c65bcf06384
Coral Fussman <coralfussman@gmail.com>
2020-03-24 04:23:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(all_emoticons)
   hash
 end
 
-def get_japanese_emoticon(load_file(./lib/emoticons.yml), english_emoticons)
+def get_japanese_emoticon(load_file./lib/emoticons.yml, english_emoticons)
 english_emoticons.each do | emoticons |
 hash["get_emoticon"][emoticons[1]] = japanese_emoticons
   end
