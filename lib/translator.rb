6701221a1009abe800e9f726e6bccc1d0e8f07ed
Coral Fussman <coralfussman@gmail.com>
2020-03-24 04:35:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ end
 
 def get_japanese_emoticon(all_emoticons = './lib/emoticons.yml', english_emoticons)
 emoticon_index = load_library(all_emoticons)
-if library["get_emoticon"].include?(english_emoticon) 
+if library["get_emoticon"].include?(english_emoticon)
     library["get_emoticon"][english_emoticon]
 
    end
