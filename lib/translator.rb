504b8f4333922ba28a2a6fe46bd3918e98ee74c0
Coral Fussman <coralfussman@gmail.com>
2020-03-24 03:28:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 require "yaml"
 
 def load_library(emoticons)
-  emoticons = YAML::load_stream(File.open('./lib/emoticons.yml'))
-  hash = {"get_emoticon": {}}
+  emoticons = YAML.load_file('./lib/emoticons.yml'))
+  hash = {"get_emoticon": {},"get_meaning" => {},}
 
   emoticons
 end
